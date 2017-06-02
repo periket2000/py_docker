0bf9203629426d0cf00892335f2bde0c7cfc2524
Marco Albero <marco@mybi.es>
2017-06-02 17:10:45 +0200

    added application environment to configure the python docker app

--- env.sh
+++ env.sh
@@ -11,3 +11,14 @@ GIT_REPO=https://github.com/periket2000/rest_api_demo.git
 #                                             +-> ...
 SRC_DIR=rest_api_demo
 APP_FILE=app.py
+
+# YOU CAN REMOVE BELOW VALUES
+# APPLICATION ENVIRONMENT (SAMPLE ENVIRONMENT VARS SUPPOSED TO BE USED BY GIT_REPO APPLICATION)
+DB_USER=pygauth
+DB_PASS=password
+DB_HOST=192.168.250.100
+DB_PORT=3306
+DB_NAME=py-gauth
+REDIS_HOST=192.168.250.100
+REDIS_PORT=6379
+REDIS_TOKEN_TIME=3600
